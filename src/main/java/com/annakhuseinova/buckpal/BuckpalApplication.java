23a6cbdf4a9d24ab02cc018e2841ecbccf3ff34d
annakhuseinova <annakhuseinova@yandex.ru>
2021-03-28 23:04:47 +0300

    Created basic structure

---
+++ src/main/java/com/annakhuseinova/buckpal/BuckpalApplication.java
@@ -0,0 +1,13 @@
+package com.annakhuseinova.buckpal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuckpalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuckpalApplication.class, args);
+	}
+
+}
